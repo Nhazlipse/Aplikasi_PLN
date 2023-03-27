66cc0c9139d6b10d0fea9c0eb1c4aaafabee731c
Nhazlipse <nhazlipse@gmail.com>
2023-03-28 00:27:08 +0700

    anas

--- menu/vendor/vendor.php
+++ menu/vendor/vendor.php
@@ -4,155 +4,127 @@ require_once '../../database/koneksi.php';
 use database\koneksi;
 $koneksi = new koneksi();
 
-include '../../layout/header.php';
+include 'layout/header.php';
 ?>
 
 <head>
 
-    <title>SB Admin 2 - Dashboard</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="../../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="../../assets/css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+<title>Arsip Online - Kediri</title>
+<link rel="stylesheet" href="layout/css/style.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">
-    <style>
-        .container1 {
-            padding-left: 0;
-            padding-right: 0;
-            width: 100%;
-            max-width: 100%;
-            box-sizing: border-box;
-        }
-
-        @media screen and (min-width: 768px) {
-            .container {
-                padding-left: 15px;
-                padding-right: 15px;
-            }
-        }
-    </style>
 </head>
 
 <body id="page-top">
 
-    <!-- Page Wrapper -->
-    <div id="wrapper">
+<!-- Page Wrapper -->
+<div id="wrapper">
 
-        <!-- Sidebar -->
-        <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar"style="background-color:#039677;">
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color:#2b2b2b;">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../../dashboard.php"style="margin-top:69px;margin-right:7px;">
-                <div class="sidebar-brand-icon">
-                <img src="../../assets/img/logo.png" width="77%">
-                </div>
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="../../dashboard.php"style="margin-top:68px;">
+<div class="sidebar-brand-icon text-center">
+<img src="../../assets/img/arsip.png" width="77%">
+</div>
+</a>
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item active"style="margin-top:68px;">
+<a class="nav-link" href="../dashboard.php">
+<i class="fas fa-fw fa-tachometer-alt"></i>
+<span>Dashboard</span></a>
+</li>
+
+<!-- DIVIDER GARIS MENUT -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+Interface
+</div>
+
+<!-- NAVIGASI MENU - Pages Collapse Menu -->
+<li class="nav-item">
+<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+aria-expanded="true" aria-controls="collapseTwo">
+<i class="fas fa-fw fa-table"></i>
+<span>Data Arsip</span>
+</a>
+<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+<div class="bg-white py-2 collapse-inner rounded">
+    <h6 class="collapse-header">Pilih Menu:</h6>
+    <a class="collapse-item" href="../data_master/data_master.php"><b>Data Master</b></a>
+    <a class="collapse-item" href="vendor.php"><b>Data Vendor</b></a>
+</div>
+</div>
+</li>
+
+<!-- DIVIDER GARIS MENU -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- SIDEBAR UNTUK MINIMIZE MENU -->
+<div class="text-center d-none d-md-inline">
+<button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+
+</ul>
+<!-- ENDING DARI BAGIAN ADMIN TOOLS -->
+
+<!-- DARI SINI ISI KONTEN NYA -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+<!-- Main Content -->
+<div id="content">
+
+<!-- Topbar -->
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+<!-- Sidebar Toggle (Topbar) -->
+<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+    <i class="fa fa-bars"></i>
+</button>
+
+<!-- Judul Menu -->
+<form
+    class="d-none d-sm-inline-block form-inline mr-auto ml-md-0 my-2 my-md-0 mw-100 navbar-search">
+    <img src="../../assets/img/tulisan.png" width="36%">
+</form>
+
+<!-- NAVIGASI BAR ATAS BAGIAN HEADER -->
+<ul class="navbar-nav ml-auto">
+    <div class="topbar-divider d-none d-sm-block"></div>
+
+    <!-- DATA BAGIAN ADMINISTRASI -->
+    <li class="nav-item dropdown no-arrow">
+        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Administrasi</span>
+            <img class="img-profile rounded-circle" src="../../assets/img/undraw_profile.svg">
+        </a>
+        <!-- ANIMASI TOMBOL LOGOUT (DROPDOWN) BAGIAN ADMINISTRASI -->
+        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+            aria-labelledby="userDropdown">
+            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                Logout
             </a>
+        </div>
+    </li>
 
+</ul>
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="../../dashboard.php"style="margin-top:68px;">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Interface
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Menu</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Pilih Menu:</h6>
-                        <a class="collapse-item" href="../data_master/data_master.php">Data Master</a>
-                        <a class="collapse-item" href="vendor.php">Vendor</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-        </ul>
-        <!-- End of Sidebar -->
-
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <form
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-0 my-2 my-md-0 mw-100 navbar-search">
-                        <img src="assets/img/dash.png" width="33%" style="margin-left:5px;">
-                    </form>
-
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
-
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                     
-
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Selamat Datang !</span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
-                            </a>
-
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
-
-                    </ul>
-
-                </nav>
-                <!-- End of Topbar -->
+</nav>
+<!-- ENDING DARI NAVIGASI BAR ATAS HEADER -->
 
                 <!-- AWAL DARI ISI KONTEN -->
+<div class="container-fluid">
+
+
+<!-- AWAL DARI ISI KONTEN -->
 <div class="container-fluid">
 
 
@@ -168,10 +140,12 @@ include '../../layout/header.php';
             <!-- Tombol Tambah Data -->
             <div class="tambahdata">
                 <a href="tambah_data_pinjaman.php" class="btn btn-primary">Tambah Data</a><br><br>
-</div>
+                </div>
                 <!-- buat kolom -->
                             <div class="table-responsive">
-                            <table id="example" class="table table-striped table-bordered" style="width:100%">
+                <!-- buat kolom -->
+                <div class="table-responsive">
+                <table id="example" class="table table-striped table-bordered" style="width:100%">
                                     <thead>
                                     <tr>
                                     <th class="text-center">No<br><br></th>
@@ -194,23 +168,14 @@ include '../../layout/header.php';
                             </thead>
                             <?php require 'panggil_data_vendor.php';?>
                             </table>
+                            <br>
                             </div>
                     </div>
-                </div>
+  </div>
+</div>
 
-            </div>
-            <!-- /.container-fluid -->
-
-            </div>
-            <!-- End of Main Content -->
-        </div>
-        <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- TOMBOL SCROLL KE ATAS-->
-    <a class="scroll-to-top rounded" href="#page-top">
+        <!-- TOMBOL SCROLL KE ATAS-->
+        <a class="scroll-to-top rounded" href="#page-top">
             <i class="fas fa-angle-up"></i>
         </a>
 
@@ -236,31 +201,13 @@ include '../../layout/header.php';
                 </div>
             </div>
         </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="../../assets/vendor/jquery/jquery.min.js"></script>
-    <script src="../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="../../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="../../assets/js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="../../assets/vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="../../assets/js/demo/chart-area-demo.js"></script>
-    <script src="../../assets/js/demo/chart-pie-demo.js"></script>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
     <script>$(document).ready(function () {
     $('#example').DataTable();
 });</script>
-
 </body>
 
 </html>
+<?php include 'layout/footer.php';?>
