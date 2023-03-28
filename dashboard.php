27a7301d713f421cfd95e90cb13e6db65a12fa29
Nhazlipse <nhazlipse@gmail.com>
2023-03-29 02:07:12 +0700

    anas

--- dashboard.php
+++ dashboard.php
@@ -154,17 +154,29 @@ include './layout/header.php';
                     <div class="row">
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
+
+                        <?php
+                        $query = "SELECT COUNT(*) as total_data FROM t_data";
+                        $koneksi = new koneksi();
+                        $result = $koneksi->query($query);
+                        $row = mysqli_fetch_assoc($result);
+                        $total_data = $row['total_data'];
+                        ?>
+
+<div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2"
+                                style="width:90%; position:relative;top:150%;left: 80%;">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1 text-center"
+                                                style="font-size:16px;">
+                                                Total Data Master</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-900 text-center">
+                                                <?php echo $total_data; ?></div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                            <i class="fa fa-id-card fa-2x text-gray-500"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -172,28 +184,29 @@ include './layout/header.php';
                         </div>
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-info shadow h-100 py-2">
+
+                        <?php
+                        $query = "SELECT COUNT(*) as total_data2 FROM t_vendor";
+                        $koneksi = new koneksi();
+                        $result = $koneksi->query($query);
+                        $row = mysqli_fetch_assoc($result);
+                        $total_data2 = $row['total_data2'];
+                        ?>
+
+<div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2"
+                                style="width:90%; position:relative;top:150%;left: 111%;">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1 text-center"
+                                                style="font-size:16px" ;>
+                                                Total Data Vendor </div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-900 text-center">
+                                            <?php echo $total_data2; ?></div>
                                             </div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                            aria-valuemax="100"></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        <i class="fa fa-folder fa-2x text-gray-500"></i>
                                         </div>
                                     </div>
                                 </div>
